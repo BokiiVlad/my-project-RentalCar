1132a63a9524e1393874a74387df87577b4d1ec9
Vlad Bokii <bokiyvladik10@gmail.com>
2025-07-25 00:06:44 +0300

    add filter

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,3 +1,61 @@
-const Filters = () => {};
+import style from "./Filters.module.css";
+import { Formik, Form, Field } from "formik";
+
+const Filters = () => {
+  const price = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200];
+  const handleSubmit = () => {};
+  return (
+    <Formik
+      initialValues={{ price: "", mileageFrom: "", mileageTo: "" }}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <div className={style.box}>
+          <div>
+            <label className={style.label} htmlFor="price">
+              Price / 1 hour
+            </label>
+            <Field
+              className={style.inputPrice}
+              as="select"
+              id="price"
+              name="price"
+            >
+              <option value="">Choose a price</option>
+              {price.map((el) => (
+                <option key={el} value={el}>
+                  ${el}
+                </option>
+              ))}
+            </Field>
+          </div>
+          <div>
+            <label className={style.label}>Car mileage / km</label>
+            <div>
+              <Field
+                className={style.inputFrom}
+                type="number"
+                name="mileageFrom"
+                placeholder="From"
+                id="mileageFrom"
+              />
+              <Field
+                className={style.inputTo}
+                type="number"
+                name="mileageTo"
+                placeholder="To"
+                id="mileageTo"
+              />
+            </div>
+          </div>
+
+          <button className={style.but} type="submit">
+            Submit
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
 
 export default Filters;
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -3,16 +3,20 @@ import CarList from "../../components/CarList/CarList.jsx";
 import { selectCar } from "../../redux/cars/selectors.js";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCar } from "../../redux/cars/operations.js";
+import Filters from "../../components/Filters/Filters.jsx";
 
 const CatalogPage = () => {
-  const dispatch = useDispatch();
-  const cars = useSelector(selectCar);
-
-  useEffect(() => {
-    dispatch(fetchCar());
-  }, [dispatch]);
-
-  return <>{cars.length > 0 ? <CarList /> : <p>Car not found.</p>}</>;
+  //   const dispatch = useDispatch();
+  //   const cars = useSelector(selectCar);
+  //   useEffect(() => {
+  //     dispatch(fetchCar());
+  //   }, [dispatch]);
+  //   return <>{cars.length > 0 ? <CarList /> : <p>Car not found.</p>}</>;
+  return (
+    <>
+      <Filters />
+    </>
+  );
 };
 
 export default CatalogPage;

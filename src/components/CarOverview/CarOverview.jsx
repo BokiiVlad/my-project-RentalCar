e7225d02fb4a1733df322e2abad26e27c9bcf24a
Vlad Bokii <bokiyvladik10@gmail.com>
2025-07-25 21:54:47 +0300

    finish detail components

--- src/components/CarOverview/CarOverview.jsx
+++ src/components/CarOverview/CarOverview.jsx
@@ -1,17 +1,31 @@
 import style from "./CarOverview.module.css";
 
 const CarOverview = ({ car }) => {
+  const parts = car.address.split(",").map((part) => part.trim());
+  const country = parts[2];
+  const city = parts[1];
+  const idSlice = car.id.slice(0, 4);
+
   return (
     <div className={style.box}>
-      <p className={style.text}>
-        {car.brand} <span className={style.span}>{car.model}</span>, {car.year}
-      </p>
-      <div>
-        <svg width="16px" height="16px">
+      <div className={style.brandBox}>
+        <h2 className={style.brand}>
+          {car.brand} <span className={style.span}>{car.model}</span>,{" "}
+          {car.year}
+        </h2>
+        <p className={style.id}>id: {idSlice}</p>
+      </div>
+      <div className={style.countryBox}>
+        <svg className={style.svg} width="16px" height="16px">
           <use href="../../../assets/sprite/sprite.svg#icon-navigation"></use>
         </svg>
-        <p></p>
+        <p className={style.textLocation}>
+          {city}, {country}
+        </p>
+        <p className={style.textMilage}>Mileage: {car.mileage} km</p>
       </div>
+      <p className={style.price}>{car.rentalPrice}$</p>
+      <p className={style.description}>{car.description}</p>
     </div>
   );
 };

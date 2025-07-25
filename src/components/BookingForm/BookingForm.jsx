33d7bd528efe7c3ad6ff2a97f4c5cc8fd9129f8b
Vlad Bokii <bokiyvladik10@gmail.com>
2025-07-25 23:24:55 +0300

    create form

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,7 +1,55 @@
+import { Field, Form, Formik } from "formik";
+import style from "./BookingForm.module.css";
+
 const BookingForm = () => {
+  const initialValues = {
+    name: "",
+    email: "",
+    comment: "",
+  };
+  const hundleSubmit = () => {};
+
   return (
     <>
-      <p>BookingForm</p>
+      <div className={style.box}>
+        <p className={style.mainText}>Book your car now</p>
+        <p className={style.text}>
+          Stay connected! We are always ready to help you.
+        </p>
+        <Formik initialValues={initialValues} onSubmit={hundleSubmit}>
+          <Form>
+            <div className={style.form}>
+              <Field
+                className={style.input}
+                name="name"
+                type="text"
+                placeholder="Name"
+              ></Field>
+              <Field
+                className={style.input}
+                name="email"
+                type="email"
+                placeholder="Email"
+              ></Field>
+              <Field
+                className={style.input}
+                name="calendar"
+                type="email"
+                placeholder="Booking Date"
+              ></Field>
+              <Field
+                className={style.inputComment}
+                name="comment"
+                type="text"
+                placeholder="Comment"
+              ></Field>
+            </div>
+            <button type="submit" className={style.button}>
+              Send
+            </button>
+          </Form>
+        </Formik>
+      </div>
     </>
   );
 };
